68e2dd2043a9e4aef390856a2ec4f5605320e033
Lindsey Smith <lindseyengineer@gmail.com>
2013-12-16 11:43:41 -0800

    can post urls to server and firebase

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express');
+var cors = require('cors');
+var Firebase = require('firebase');
+var Users = new Firebase('https://leaena.firebaseio.com/users');
+var Websites = new Firebase('https://leaena.firebaseio.com/websites');
+Websites.set("hello world!");
 
 var app = express();
 
 app.use(express.static(__dirname + '/app'));
+app.use(express.bodyParser());
 
-var port = process.env.PORT || 3000
+app.use(cors());
+app.use(app.router);
+
+app.get('/websites', function(req, res){
+  Websites.on('value', function(snapshot) {
+    var JSON = snapshot.val();
+    res.json(JSON);
+  });
+});
+
+app.post('/websites', function(req, res){
+    console.log(req.body.url);
+    Websites.push({URL: req.body.url});
+    res.redirect('/');
+});
+
+var port = process.env.PORT || 3000;
 app.listen(port, function(){
   console.log("listening on " + port);
 });
